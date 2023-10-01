10e3da95ecfbed12fb4e1a315f4f7dcfadb9520a
shardulchavan <chavan.shardul360@gmail.com>
2023-10-01 14:12:32 -0400

    Edited python code to create datatype txt files

--- Part-2/streamlit/DataPreprocessing.py
+++ Part-2/streamlit/DataPreprocessing.py
@@ -4,7 +4,9 @@ import streamlit as st
 
 from ydata_profiling import ProfileReport as pr
 from streamlit_pandas_profiling import st_profile_report
-sheet_name="Monthly"
+
+# change the sheet name as per data being processed
+sheet_name="Origination"
 # Load the Excel file into a DataFrame
 df = pd.read_excel('/home/shardulc/DAMG7245/Assignment1/Part-2/streamlit/file_layout.xlsx',sheet_name = sheet_name, skiprows=1)
 
@@ -22,10 +24,20 @@ for index, row in df.iterrows():
     dtype_dict[attribute_name] = data_type_format
     
     # Check if the data type is "Numeric" and set to "int"
-    if "Numeric" in data_type_format:
-        dtype_dict[attribute_name] = "int"
+    if data_type_format.startswith("Numeric"):
+        # Split the format into parts
+        parts = data_type_format.split('-')
+        
+        if len(parts) == 2:
+            # Extract precision and scale if available
+            precision, scale = map(int, parts[1].split(','))
+            dtype_dict[attribute_name] = f"float({precision},{scale})"
+        else:
+            # If precision and scale are not specified, set as "int"
+            dtype_dict[attribute_name] = "int"
+
     # Check if the data type is "Alpha" and set to "str"
-    elif "Alpha" in data_type_format:
+    elif "Alpha" or "Alpha Numeric" or " Alpha-Numeric" in data_type_format:
         dtype_dict[attribute_name] = "str"
     else:
         # If neither "Numeric" nor "Alpha," you can handle other cases here
@@ -34,14 +46,7 @@ for index, row in df.iterrows():
 # Now, dtype_dict contains the attribute names as keys and their data types & formats as values
 print(dtype_dict)
 
-  
-with open('monthly_dtypes.txt','w') as data: 
+# Change the file name before writing the txt file 
+with open('origination_dtypes.txt','w') as data: 
       data.write(str(dtype_dict))
 
-# file_path = '/home/shardulc/DAMG7245/ydataProfile/sample_orig_2021.csv'
-# df = pd.read_csv(file_path, dtype=dtype_dict)
-# print(df.head(10))
-
-# profile = pr(df,tsmode=True, ,explorative=True)
-
-# profile = ProfileReport(df, title="Profiling Report",explorative=True)

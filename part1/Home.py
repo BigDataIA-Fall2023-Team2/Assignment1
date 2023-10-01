3e7ac013f8fa6ca2b57ca4530144f02ad8f05f3c
Dhawal negi <dhawal.negi1@gmail.com>
2023-10-01 11:10:46 -0400

    Code refactoring, removed file write

--- part1/Home.py
+++ part1/Home.py
@@ -5,6 +5,7 @@ from pypdf import PdfReader
 import subprocess
 import time
 import os
+import io
 
 st.title("Securities and Exchange commision form summarizer")
 
@@ -22,42 +23,28 @@ if library_option == 'Nougat':
 
 if st.button("Run Command"):
     if input_type_option == 'Link':
-        file_path=''
-        if file_link is not "":
-            random_uuid = uuid.uuid4()
-            file_directory = subprocess.check_output("pwd", shell=True, text=True)[:-1]+"/file"
-            os.makedirs(file_directory, exist_ok=True)
-            file_path = file_directory+"/"+str(random_uuid)+".pdf"
+        if file_link != '':
             try:
-                response = requests.get(file_link)
-                response.raise_for_status()  # Check for any HTTP errors.
-                with open(file_path, 'wb') as file:
-                    file.write(response.content)
-
-                print(f"File downloaded and saved as {file_path}")
+                file_response = requests.get(file_link)
+                file_response.raise_for_status()  # Check for any HTTP errors.
+                file_content = file_response.content
             except requests.exceptions.RequestException as e:
                 st.error("Error in fetching file from link. Please provide a valid link to any Securities and Exchange commision form.")
                 print(f"Error: {e}")
         else:
             st.error("Please provide a link to any Securities and Exchange commision form.")
     else:
-        file_path=''
         if uploaded_file is not None:
-            random_uuid = uuid.uuid4()
-            file_directory = subprocess.check_output("pwd", shell=True, text=True)[:-1]+"/file"
-            os.makedirs(file_directory, exist_ok=True)
-            file_path = file_directory+"/"+str(random_uuid)+".pdf"
-            with open(file_path, "wb") as pdf_file:
-                pdf_file.write(uploaded_file.read())
+            file_content = uploaded_file.read()
         else:
             st.error("Please upload any Securities and Exchange commision form in pdf format.")
     if library_option == "Nougat":
-        if collab_link is not "":
-            if file_path is not "":
+        if collab_link != "":
+            if ((input_type_option == 'Link' and file_link != '') or (input_type_option == 'Upload a PDF' and uploaded_file is not None)):
                 start_time = time.time()
-                with open(file_path, 'rb') as pdf_file:
-                    reader = PdfReader(pdf_file)
-                    number_of_pages  = len(reader.pages)
+                
+                reader = PdfReader(io.BytesIO(file_content))
+                number_of_pages  = len(reader.pages)
                 st.write("Number of pages in the PDF are: " + str(number_of_pages))
                 headers = {
                     'accept': 'application/json',
@@ -68,16 +55,19 @@ if st.button("Run Command"):
                         'start': page_num,  
                         'stop': page_num
                     }
+                    random_uuid = uuid.uuid4()
                     files = {
-                     'file': (str(random_uuid) + "_" +str(page_num)+ ".pdf", open(file_path, 'rb'), 'application/pdf')
-                }
-                    response = requests.post(collab_link+"/predict", headers=headers, files=files, params=params)
-                    if response.status_code == 200:
+                    'file': (str(random_uuid) + "_" +str(page_num), file_content, 'application/pdf')
+                    }
+                    nougat_response = requests.post(collab_link+"/predict", headers=headers, files=files, params=params)
+                    if nougat_response.status_code == 200:
                         st.write("Page No: " + str(page_num))
-                        st.write(response.text)
+                        st.write(nougat_response.text)
                     else:
-                        st.error("Request failed with status code:" + str(response.status_code))
-                        st.error(response.text)
+                        st.error("Request failed with status code:" + str(nougat_response.status_code))
+                        st.error(nougat_response.text)
+                        st.error("Please check your localtunnel to nougat_api on google collab!!!")
+                        break
                 end_time = time.time()
                 st.subheader("Time taken")
                 st.text(end_time - start_time )
@@ -86,28 +76,21 @@ if st.button("Run Command"):
         else:
             st.error("Please provide a localtunnel link to google collab running nougat_api.")
     else:
-        if file_path is not "":
+        if ((input_type_option == 'Link' and file_link != '') or (input_type_option == 'Upload a PDF' and uploaded_file is not None)):
             start_time = time.time()
             num_words=0
-            with open(file_path, 'rb') as pdf_file:
-                reader = PdfReader(pdf_file)
-                number_of_pages  = len(reader.pages)
-                for page_num in range(number_of_pages):
-                    page = reader.pages[page_num]
-                    st.write("Page No: " + str(page_num+1))
-                    extracted_text = page.extract_text()
-                    st.write(extracted_text)
-                    num_words += len(extracted_text.split())
-                st.subheader("Number of Words")
-                st.text(num_words)
+            reader = PdfReader(io.BytesIO(file_content))
+            number_of_pages  = len(reader.pages)
+            for page_num in range(number_of_pages):
+                page = reader.pages[page_num]
+                st.write("Page No: " + str(page_num+1))
+                extracted_text = page.extract_text()
+                st.write(extracted_text)
+                num_words += len(extracted_text.split())
+            st.subheader("Number of Words")
+            st.text(num_words)
             end_time = time.time()
             st.subheader("Time taken")
             st.text(end_time - start_time)
         else:
             pass
-            
-    
-    
-    
-    
-    

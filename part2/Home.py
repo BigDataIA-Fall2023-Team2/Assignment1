55be4b4bd0aa99fbd7aef0448ce9258f7e687fa7
Dhawal negi <dhawal.negi1@gmail.com>
2023-10-05 18:36:38 -0400

    Update Home.py

--- part2/Home.py
+++ part2/Home.py
@@ -23,8 +23,8 @@ def profile_data(df):
         
         
 def process_file(uploaded_file, datatype_option):
-    st.write(os.getcwd())
-    os.chdir("./part2")
+    if os.getcwd().split('/')[-1] is not "part2":  #added to fix the directory issue on streamlit cloud deployment
+        os.chdir("./part2")
     with open("./"+datatype_option.lower() + '_columns.txt', 'r') as file:
         column_names = [line.strip() for line in file]
     if uploaded_file.name.split('.')[1].lower() == "csv":

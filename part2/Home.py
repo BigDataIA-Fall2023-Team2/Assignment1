d12b0b8138fa11750679fb7e6477ce15a8721684
Dhawal negi <dhawal.negi1@gmail.com>
2023-10-04 22:18:48 -0400

    Update code to run gx

--- part2/Home.py
+++ part2/Home.py
@@ -1,93 +1,86 @@
 import streamlit as st
 import pandas as pd
 from ydata_profiling import ProfileReport as pr
-from streamlit_pandas_profiling import st_profile_report
+# from streamlit_pandas_profiling import st_profile_report
 import great_expectations as gx
-from great_expectations.checkpoint import SimpleCheckpoint
-from great_expectations.core.batch import Batch, BatchRequest, RuntimeBatchRequest
-from great_expectations.validator.validator import Validator
-from great_expectations.execution_engine import PandasExecutionEngine
-import json
-import yaml
-
-# from great_expectations.data_context import DataContext
+import uuid
+import streamlit.components.v1 as components
 
 
+def validate_data(df, datatype_option, filename):
+    context = gx.data_context.DataContext("./gx")
+    datasource = context.get_datasource("pandas_dataframe_datasource")
+    random_uuid = uuid.uuid4()
+    asset_name="asset_"+filename.split('.')[0].lower() +"_" +str(random_uuid)
+    data_asset = datasource.add_dataframe_asset(name=asset_name)
+    batch_request = data_asset.build_batch_request(dataframe=df)
+    checkpoint=context.add_or_update_checkpoint(
+        run_name_template=asset_name,
+        name = "my_checkpoint",
+        validations = [
+            {
+                "assest_name": asset_name,
+                "batch_request": batch_request,
+                "expectation_suite_name": datatype_option.lower() +"_expectations_suite"
+            }
+        ],
+    )
+    checkpoint_result = checkpoint.run(run_name="test")
+    context.build_data_docs()
+    with open("./gx/uncommitted/data_docs/local_site/index.html", "r") as file:
+        html_content = file.read()
+    components.html(html_content, height=800, width=1000, scrolling=True)
+    st.download_button(
+        label="Download validation report",
+        data=html_content.encode("utf-8"),
+        file_name="vlidation_report.html",
+        mime="text/html"
+    )
+
+        
+def profile_data(df):
+    profile = pr(df, explorative=True)
+    st.write("Data Summary:")
+    st.title("Pandas Profiling Report")
+    # st_profile_report(profile)
+    profile_html = profile.to_html()
+    components.html(profile_html, height=800, width=800, scrolling=True)
+    st.download_button(
+        label="Download profiling report",
+        data=profile_html.encode("utf-8"),
+        file_name="profiling_report.html",
+        mime="text/html"
+    )
+        
+        
+def process_file(uploaded_file, datatype_option):
+    with open(datatype_option.lower() + '_columns.txt', 'r') as file:
+        column_names = [line.strip() for line in file]
+    if uploaded_file.name.split('.')[1].lower() == "csv":
+        df = pd.read_csv(uploaded_file)
+    else:
+        df = pd.read_excel(uploaded_file)
+    df.columns=column_names
+    if datatype_option == 'Monthly':
+        date_type_columns=['Monthly Reporting Period','Defect Settlement Date','Zero Balance Effective Date','Due Date of Last Paid Installment (DDLPI)']
+    else:
+        date_type_columns=['First Payment Date','Maturity Date']
+    for date_type_column in date_type_columns:
+        df[date_type_column] = pd.to_datetime(df[date_type_column]).dt.strftime('%Y%m')
+    return df
+        
+        
 st.title("Freddie mac single family dataset quality evaluation and summarization")
 st.write("Please upload the dataset in csv or xls file format and indicate if it is Origination/Monthly performance data")
 
-# Upload a file
 datatype_option = st.selectbox("Select an option!", ('Origination', 'Monthly'))
 uploaded_file = st.file_uploader("Please uplaod Freddie mac single family dataset for quality evaluation and summarization.", type=['csv','xls'])
-
-
 
 if st.button("Submit"):
     if uploaded_file is None:
         st.error("Please upload " + datatype_option +  " dataset in csv or xls format.")
-        exit()
     else:
-        with open(datatype_option.lower() + '_columns.txt', 'r') as file:
-            column_names = [line.strip() for line in file]
-        if uploaded_file.name.split('.')[1].lower() == "csv":
-            df = pd.read_csv(uploaded_file)
-        else:
-            df = pd.read_excel(uploaded_file)
-        df.columns=column_names
-        if datatype_option == 'Monthly':
-            date_type_columns=['Monthly Reporting Period','Defect Settlement Date','Zero Balance Effective Date','Due Date of Last Paid Installment (DDLPI)']
-        else:
-            date_type_columns=['First Payment Date','Maturity Date']
-        for date_type_column in date_type_columns:
-            df[date_type_column] = pd.to_datetime(df[date_type_column]).dt.strftime('%Y%m')
-        st.write(df.head(5))
-        
-        # suite = context.get_expectation_suite(origination_expectations_suit)  # Replace with your suite name
-        # is_running = suite.is_expectation_suite_configured()
-        # if is_running:
-        #     st.success("Great Expectations expectations are running.")
-        # else:
-        #     st.error("Great Expectations expectations are not configured.")
-
-
-        # profile = pr(df, explorative=True)
-        # st.write("Data Summary:")
-        # st.title("Pandas Profiling Report")
-        # st_profile_report(profile)
-        
-        
-        context = gx.data_context.DataContext("./gx/")
-        execution_engine = PandasExecutionEngine()
-        expectation_suite = context.get_expectation_suite("origination_expectations_suit.json")
-        batch = Batch(
-            data=df,
-            batch_request={
-                "datasource_name": "temporary",
-                "data_connector_name": "default_inferred_data_connector_name",
-                "data_asset_name": "temp_asset",
-                "partition_request": {
-                    "batch_identifiers": {
-                        "id": "temporary"
-                    }
-                },
-                "expectation_suite_name": "origination_expectations_suit"  # Include this line
-            }
-        )
-        validator = Validator(
-            execution_engine=execution_engine,
-            batches=(batch,),
-            expectation_suite=expectation_suite,
-        )
-        results = validator.validate()
-
-        results_json_str = json.dumps(results.to_json_dict(), indent=4)
-        st.write(results_json_str)
-        st.download_button(
-            label="Download Results",
-            data=results_json_str,
-            file_name='validation_results.json',
-            mime='application/json'
-        )
-        
-        context.build_data_docs()
-        context.open_data_docs()
+        filename = uploaded_file.name
+        df=process_file(uploaded_file, datatype_option)
+        profile_data(df)
+        validate_data(df, datatype_option, filename)
--- part2/expectation_suits_builder.py
+++ part2/expectation_suits_builder.py
@@ -1,67 +1,58 @@
-import great_expectations as gx
 from great_expectations.core.expectation_configuration import ExpectationConfiguration
-import pandas as pd
+from great_expectations.data_context import FileDataContext
+from great_expectations.core.expectation_configuration import ExpectationConfiguration
 import json
-import os
-import subprocess
-
-
-subprocess.run(["great_expectations", "init"], cwd='./.')   
-context = gx.data_context.DataContext("./gx")
-datasource_config = {
-        "name": "my_datasource",
-        "class_name": "PandasDatasource",
-        "data_asset_type": {
-            "class_name": "PandasDataset"
-        },
-        "batch_kwargs_generators": {}
-    }
-context.add_datasource(**datasource_config)
 
 with open("expectations.json", "r") as expectations_file:
     expectations = json.load(expectations_file)
     
-origination_expectations_suite = context.create_expectation_suite("origination_expectations_suit")
+context = FileDataContext.create(project_root_dir="./.")
+datasource = context.sources.add_pandas("pandas_dataframe_datasource")
 
+origination_expectations_suite = context.create_expectation_suite("origination_expectations_suite")
+with open('origination_columns.txt', 'r') as file:
+    column_names = [line.strip() for line in file]
+ordered_list_origination_expectation_configuration = ExpectationConfiguration(
+    expectation_type="expect_table_columns_to_match_ordered_list",
+    kwargs={
+        "column_list": column_names
+    },
+)
+origination_expectations_suite.add_expectation(expectation_configuration=ordered_list_origination_expectation_configuration)
 for expectation in expectations['origination_expectations']:
-    column = expectation['column']
-    expectation_type = expectation['expectation_type']
-    kwargs = expectation['kwargs']
     expectation_config = ExpectationConfiguration(
-            expectation_type=expectation_type,
-            kwargs={"column": column, **kwargs}
+            expectation_type=expectation['expectation_type'],
+            kwargs=expectation['kwargs']
     )
     origination_expectations_suite.add_expectation(expectation_config)
+context.save_expectation_suite(origination_expectations_suite)
 
-# with open('origination_columns.txt', 'r') as file:
-#     column_names = [line.strip() for line in file]
-# valid_column_order_expecation = ExpectationConfiguration(
-#     expectation_type="expect_table_columns_to_match_ordered_list",
-#     kwargs={
-#         "column_list": column_names}
-# )
-# origination_expectations_suit.add_expectation(valid_column_order_expecation)
-context.save_expectation_suite(origination_expectations_suite, "origination_expectations_suit.json")
 
-# monthly_expectations_suite = context.create_expectation_suite("monthly_expectations_suit")
+monthly_expectations_suite = context.create_expectation_suite("monthly_expectations_suite")
+with open('monthly_columns.txt', 'r') as file:
+    column_names = [line.strip() for line in file]
+ordered_list_monthly_expectation_configuration = ExpectationConfiguration(
+    expectation_type="expect_table_columns_to_match_ordered_list",
+    kwargs={
+        "column_list": column_names
+    },
+)
+monthly_expectations_suite.add_expectation(expectation_configuration=ordered_list_monthly_expectation_configuration)
+for expectation in expectations['monthly_expectations']:
+    expectation_config = ExpectationConfiguration(
+            expectation_type=expectation['expectation_type'],
+            kwargs=expectation['kwargs']
+    )
+    origination_expectations_suite.add_expectation(expectation_config)
+context.save_expectation_suite(monthly_expectations_suite)
 
-# for expectation in expectations['monthly_expectations']:
-#     print(expectation)
-#     column = expectation['column']
-#     expectation_type = expectation['expectation_type']
-#     kwargs = expectation['kwargs']
-#     expectation_config = ExpectationConfiguration(
-#             expectation_type=expectation_type,
-#             kwargs={"column": column, **kwargs}
-#     )
-#     origination_expectations_suite.add_expectation(expectation_config)
+config = context.get_config()
+print(config["data_docs_sites"]["local_site"]["store_backend"]["base_directory"])
+config["data_docs_sites"]["local_site"]["store_backend"]["base_directory"]= "./temp_validation/"
+print(config["data_docs_sites"]["local_site"]["store_backend"]["base_directory"])
 
-# # with open('monthly_columns.txt', 'r') as file:
-# #     column_names = [line.strip() for line in file]
-# # valid_column_order_expecation = ExpectationConfiguration(
-# #     expectation_type="expect_table_columns_to_match_ordered_list",
-# #     kwargs={
-# #         "column_list": column_names}
-# # )
-# # monthly_expectations_suit.add_expectation(valid_column_order_expecation)
-# context.save_expectation_suite(monthly_expectations_suite, "monthly_expectations_suit.json")
+context.set_config(config)
+
+
+config = context.get_config()
+print(config["data_docs_sites"]["local_site"]["store_backend"]["base_directory"])
